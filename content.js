61c34f569a00e20e8c1b8130f45f3c55c0991287
Jason M <46291780+JasonMorta@users.noreply.github.com>
2022-12-15 21:47:08 +0200

    dones

--- content.js
+++ content.js
@@ -26,15 +26,16 @@ function getStorage() {
   }
 }
 
-//save custom snippet to local storage
+//SAVE custom snippet to local storage
 let saveBtn = document.querySelector("#save-btn")
 saveBtn.addEventListener("click", function () {
   snippetArray.unshift(textBox.value);
   localStorage.setItem("snippet", JSON.stringify(snippetArray));
   snips()
+  textBox.value = ""
 });
 
-//creates snippet list
+//CREATE snippet list
 function snips() {
     list.innerHTML = ""
 
@@ -47,7 +48,8 @@ function snips() {
         snip.className = "my-snippet";
         snip.textContent = snippetArray[i];
         snipCon.appendChild(snip);
-        //dele snippet btn
+
+        //DELETE snippet btn
         let del = document.createElement("button");
         del.textContent = "del";
         del.className = "delete-snip snip-btn";
@@ -60,8 +62,7 @@ function snips() {
         });
         snipCon.appendChild(del);
     
-
-        //copy text to clipboard
+        //COPY text to clipboard
         let copy = document.createElement("button")
         copy.className = "copy-snip snip-btn"
         copy.textContent = "copy";
@@ -70,6 +71,40 @@ function snips() {
           navigator.clipboard.writeText(snippetArray[i]);
         })
         snipCon.appendChild(copy);
+
+        //MOVE snip up btn
+        let up = document.createElement("img")
+        up.src="./up.png"
+        up.className="move-snip snip-btn"
+        up.alt="up"
+        up.addEventListener('click', ()=>{
+       
+            //get selected snip index
+            let fromIndex = snippetArray.indexOf(snippetArray[i])//index number
+
+            //extract the selected snip
+            let extracted = snippetArray.splice(fromIndex, 1, snippetArray[i]);
+      
+            //prevent reaching into index -1
+            if (!fromIndex-1 == -1) {
+            //replace selected index item with the previous index item,
+            //making both the same
+            snippetArray.splice(fromIndex, 1, snippetArray[fromIndex-1]);
+
+            //when selecting an item, target the previous ones index,
+            //and replace it with extracted snip
+            snippetArray.splice(fromIndex-1, 1, extracted);
+            }
+   
+ 
+      
+            //Cal updated list
+            localStorage.setItem("snippet", JSON.stringify(snippetArray));
+            getStorage()
+     
+        })
+        snipCon.appendChild(up);
+
         list.appendChild(snipCon);
       }
 
